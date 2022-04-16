a4a5ac22648630fd6e43f9ace4c3c3265374c0d0
Barashek-1507 <v_ergasheva@mail.ru>
2022-04-16 15:27:59 +0300

    auth logic

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const {port} = require('./config')
+const authRouter = require('./routes/authRouter')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/auth', authRouter);
+
 const start = () => {
     try{
-        app.listen(5000, () => console.log('Server has bbeen started on port 5000'))
+        app.listen(port, () => console.log(`Server has bbeen started on port ${port}`))
     } catch (e){
         console.log(e)
     }
 }
 
 start();
+module.exports = app;
